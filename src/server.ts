1b1cfc3b0b7f1157d8ca283eb3f15cf963ea992f
Johannes Jaeger <SESA480227@se.com>
2019-12-09 11:11:37 +0100

    use apollo server

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,22 @@
 
 import { Db, MongoClient } from 'mongodb';
 import * as express from 'express';
-import * as graphqlHTTP from 'express-graphql';
-import { UserModule } from './modules/user';
 import { initAppModule } from './app-module';
+import { ApolloServer } from 'apollo-server';
 
 export async function initServer() {
     const connection = await MongoClient.connect("mongodb://localhost:27018".split('==').join('%3D%3D'), { useNewUrlParser: true });
     const db: Db = connection.db("digital-services123");
 
-    const app = express();
-
     const { schemaAsync, injector } = await initAppModule({ db });
-
     const schema = await schemaAsync;
-    app.use('/graphql', graphqlHTTP({
-        schema,
-        graphiql: true
-    }));
 
-    app.listen(4001)
+    const server = new ApolloServer({
+        schema,
+        context: session => session
+    });
+
+    server.listen(4001).then(({ url }) => {
+        console.log(`🚀  Server ready at ${url}`);
+    });
 }
